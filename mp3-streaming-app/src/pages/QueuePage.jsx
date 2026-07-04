c3776d47ed60d365c57bf7b81a5d716fa2424998
Krystyn Kevin <252412757+Krystyn-Kevin@users.noreply.github.com>
2026-07-04 15:43:43 +0530

    Delete mp3-streaming-app directory

--- mp3-streaming-app/src/pages/QueuePage.jsx
+++
@@ -1,9 +0,0 @@
-import QueuePanel from "../components/queue/QueuePanel";
-
-export default function QueuePage() {
-  return (
-    <div className="mx-auto max-w-xl">
-      <QueuePanel />
-    </div>
-  );
-}
